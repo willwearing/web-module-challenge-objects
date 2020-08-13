9357e9758d8826a6a6b6fd7829aed67692c8ce3f
willwearing <willwearing@gmail.com>
2020-08-13 14:33:30 -0600

    updated first task to correctly include intergers and not strings

--- index.js
+++ index.js
@@ -132,13 +132,13 @@ and should return a string in the format `{name} gave the restaurant a {rating},
  * it will return `Daniela gave the restaurant a 5 star review and their feedback was: Beautiful atmosphere and wonderful vegan options!`
 */
 
-function getReviewByIndex(reviews, index) {
+function getReviewByIndex(arr, index) {
   console.log(
-    `${reviews[index].name} gave the restaurant a ${reviews[index].rating} and their feedback was ${reviews[index].feedback}`
+    `${arr[index].name} gave the restaurant a ${arr[index].rating} and their feedback was ${arr[index].feedback}`
   );
 }
 
-getReviewByIndex(reviews, 2);
+getReviewByIndex(reviews, 0);
 
 /* Task 7: Write a function to get information about the most recent review called `getLastReview`
 
